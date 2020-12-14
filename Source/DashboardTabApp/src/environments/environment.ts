f1d1a25c60318c8d7c95c5e1125e1bfae1c515f7
iamamangosteen <63034694+iamamangosteen@users.noreply.github.com>
2020-12-15 00:19:33 +0800

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://tt-qbot.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://tinkertanker-qbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
